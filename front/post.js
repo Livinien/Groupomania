b55daaae7d488c76d69a11649b0979f8ee1dae65
Flavien <60539775+Livinien@users.noreply.github.com>
2021-08-16 17:30:40 +0200

    Add files via upload

--- front/post.js
+++ front/post.js
@@ -1,5 +1,9 @@
 
 
+
+
+// CODE 1 //
+
 // let form = document.getElementById("form");
 
 
@@ -21,94 +25,96 @@
 
 
 
+
+    // CODE 2 //
+
+
     // SIGNUP //
 
-    const postForm = document.getElementById("postForm");
+    // const postForm = document.getElementById("postForm");
 
 
-    postForm.addEventListener("submit", async (e) => {
-        e.preventDefault()
+    // postForm.addEventListener("submit", async (e) => {
+    //     e.preventDefault()
     
-        const post = { 
+    //     const post = { 
     
-            title: document.getElementById("title").value, 
-            content: document.getElementById("content").value, 
+    //         title: document.getElementById("title").value, 
+    //         content: document.getElementById("content").value, 
             
-        } 
+    //     } 
     
-        console.log(post);
+    //     console.log(post);
         
         
-        let isValidTitle = false
-        let isValidContent = false
+    //     let isValidTitle = false
+    //     let isValidContent = false
         
         
         
 
         
-            // FIRSTNAME //
+    //         // FIRSTNAME //
     
         
-        if(post.title) {
+    //     if(post.title) {
     
-        if(post.title.length === 0) {
-            console.log(post.title);
-            
-    
-        }
-            
-        if(post.title.length < 3) {
-            console.log(post.title);
+    //     if(post.title.length === 0) {
+    //         console.log(post.title);
             
     
-        } else {
-            isValidTitle = true
-            console.log(isValidTitle);
-        } 
+    //     }
+            
+    //     if(post.title.length < 3) {
+    //         console.log(post.title);
+            
     
-    } 
+    //     } else {
+    //         isValidTitle = true
+    //         console.log(isValidTitle);
+    //     } 
+    
+    // } 
     
     
-            // LASTNAME //
+    //         // LASTNAME //
     
         
-        if(post.content.length === 0) {
-            console.log(post.content);
+    //     if(post.content.length === 0) {
+    //         console.log(post.content);
             
-        }
+    //     }
     
-        if(post.content) {
+    //     if(post.content) {
     
             
-        if(post.content.length < 3) {
-            console.log(post.content);
+    //     if(post.content.length < 3) {
+    //         console.log(post.content);
 
     
-        } else {
+    //     } else {
     
-            isValidContent = true
-            console.log(isValidContent);
+    //         isValidContent = true
+    //         console.log(isValidContent);
     
-        }
+    //     }
     
-    }
-    
-    
+    // }
     
     
-            // CONFIRMER LA REQUÊTE //
+
     
-        if(isValidTitle && isValidContent) {
+    //         // CONFIRMER LA REQUÊTE //
     
-            const json = post(post)
-            console.log(json);
+    //     if(isValidTitle && isValidContent) {
     
-        }
+    //         const json = post(post)
+    //         console.log(json);
+    
+    //     }
     
     
-    });
-    
-    
+    // });
     
     
     
@@ -116,161 +122,80 @@
 
 
 
+// CODE 3 //
 
-// var post;
-// var posts = [];
-// var posts2;
-// var userPost = document.getElementById("post");
+var selectedRow = null
 
+function onFormSubmit() {
 
-// document.getElementById("postForm").addEventListener("submit", (e) => {
-
-//     e.preventDefault(); 
-
-//     Create();
-//     Read();
-//     document.getElementById("form").reset();
-
-// });
+    var formData = readFormData();
+    if(selectedRow == null)
+    insertNewRecord(formData);
+    else
+    updateRecord(formData);
+    resetForm();
+}
 
 
-// function Create() {
+function readFormData() {
 
-//     let storage = JSON.parse(localStorage.getItem("title"))
-//     post = document.getElementById('title').value;
+    var formData = {};
+    formData["title"] = document.getElementById("title").value;
+    formData["content"] = document.getElementById("content").value;
 
-//     if(post == '') {
+    return formData;
 
-//         alert('Ecrit le nom de ton post');
-
-//     } else {
-    
-//         if (storage == null) {
-
-//             posts.push(post);
-//             localStorage.setItem("posts", JSON.stringify(posts));
-
-//         } else {
-
-//             posts = JSON.parse(localStorage.getItem("posts"));
-//             posts.push(post);
-//             localStorage.setItem("posts", JSON.stringify(posts));
+}
 
 
-//         }
-//     }
+function insertNewRecord(data) {
 
-// }
+    var newArticle = document.getElementById("post").getElementsByTagName("tbody")[0];
 
-// function Read() {
+    var newRow = newArticle.insertRow(newArticle.length);
 
-//     userPost.innerHTML = '';
-//     posts2 = JSON.parse(localStorage.getItem("posts"));
+    cell1 = newRow.insertCell(0);
+    cell2 = newRow.insertCell(1);
+    cell2 = newRow.insertCell(2);
+    cell2.innerHTML = `<div class="card"">
+    <div class="card-body">
+      <h2 class="card-title">${data.title}</h2>
+      <p class="card-text">${data.content}</p>
+      <button onClick="onEdit(this)" type="button" class="btn btn-primary">Modifier</button>
+      <button onClick="onDelete(this)" type="button" class="btn btn-danger">Supprimer</button>
+    </div>
+  </div>`;
 
-//     if(posts2 == null) {
+}
 
-//         userPost.innerHTML+= `
-        
-//         <div class="card" style="width: 18rem;">
-//             <div class="card-body">
-//                 <h5 class="card-title">Card title</h5>
-//                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                 <a href="#" class="card-link">Card link</a>
-//                 <a href="#" class="card-link">Another link</a>
-//             </div>
-//         </div>
-        
-//         `
+function resetForm() {
 
-//     } else {
+    document.getElementsById("title").value = "";
+    document.getElementsById("content").value = "";
+    selectedRow = null;
+}
 
-//         for(var i = 0; i < posts2.length; i++) {
+function onEdit(td) {
 
-//             userPost.innerHTML+= `
-        
-//             <div class="card" style="width: 18rem;">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${i+1}</h5>
-//                     <h6 class="card-subtitle mb-2 text-muted">${posts2[i]}</h6>
-//                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                     <button Onclick="Delete(${i})" type="button" class="btn btn-primary">Modifier</button>
-//                     <button Onclick="Delete(${i})" type="button" class="btn btn-danger">Supprimer</button>
-                    
-//                 </div>
-//             </div>
-//             `
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("title").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("content").value = selectedRow.cells[1].innerHTML;
 
-//         }
+}
 
-//     }
+function updateRecord(formData) {
 
-// }
+    selectedRow.cells[0].innerHTML = formData.title;
+    selectedRow.cells[1].innerHTML = formData.content;
+
+}
 
 
-// function Update(i3) {
+function onDelete(td) {
 
-//     let posts4 = JSON.parse(localStorage.getItem("posts"));
-//     userPost.innerHTML = '';
-//     userPost.innerHTML += `
-    
-//     <div class="card" style="width: 18rem;">
-//             <div class="card-body">
-//                 <h5 class="card-title">Card title</h5>
-//                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                 <a href="#" class="card-link">Card link</a>
-//                 <a href="#" class="card-link">Another link</a>
-//             </div>
-//         </div>
+    if(confirm('Êtes vous sur de vouloir supprimer ce post ?'))
+    row = td.parentElement.parentElement;
+    document.getElementById("post").deleteRow(row.rowIndex);
+    resetForm();
 
-//     `
-
-//     for(var i = 0; i < posts4.length; i++) {
-
-//         if(i == i3) {
-            
-//             userPost.innerHTML+= `
-            
-//             <div class="card" style="width: 18rem;">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${i+1}</h5>
-//                     <h6 class="card-subtitle mb-2 text-muted">${posts4[i]}</h6>
-//                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                     <button Onclick="Update2(${i})" type="button" class="btn btn-primary">Modifier</button>
-//                     <button Onclick="Read()" type="button" class="btn btn-danger">Supprimer</button>
-                    
-//                 </div>
-//             </div>
-//             `
-
-
-//         } else {
-
-//             userPost.innerHTML+= `
-        
-//             <div class="card" style="width: 18rem;">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${i+1}</h5>
-//                     <h6 class="card-subtitle mb-2 text-muted">${posts2[i]}</h6>
-//                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                     <button Onclick="Update(${i})" type="button" class="btn btn-primary">Modifier</button>
-//                     <button Onclick="Delete(${i})" type="button" class="btn btn-danger">Supprimer</button>
-                    
-//                 </div>
-//             </div>
-//             `
-            
-
-//         }
-
-//     }
-
-// }
-
-
-
-
-
-
-
+}
