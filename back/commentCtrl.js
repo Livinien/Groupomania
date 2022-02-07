baebf8b3b3c6149acadbc78a3368e2ee13fcb322
Flavien <60539775+Livinien@users.noreply.github.com>
2022-02-07 17:29:48 +0100

    Add files via upload

---
+++ back/commentCtrl.js
@@ -0,0 +1,191 @@
+const db = require("../models");
+const jwt = require("../middleware/jwt");
+
+
+
+// CREATION DE COMMENTAIRE //
+
+exports.createComment = async (req, res) => {
+
+    try {
+
+        const PostId = req.body.PostId
+        const content = req.body.content;
+        const UserId = await jwt.getUserId(req);
+
+        if(!content) {
+
+            return res.status(400).json({ error: "Informations manquantes"});
+
+        }
+
+        let Comment = await db.Comment.create({
+
+            content,
+            UserId,
+            PostId,
+            
+
+        });
+
+        return res.status(201).json({ message: "Commentaire Ajouté !", Comment});
+        
+    } catch (error) {
+
+        return res.status(500).json({ message: error.message });
+        
+    }
+
+}
+
+
+
+
+
+
+// AFFICHER TOUS LES COMMENTAIRES //
+
+exports.getAllComment = async (req, res) => {
+
+    try {
+
+        let PostId = req.params.id
+        const Comment = await db.Comment.findAll({
+
+            where : { 
+                PostId: PostId },
+                include: [{ 
+                    model: db.User, 
+                    attributes: ['firstname'] }],
+                order: [["createdAt", "DESC"]]
+            
+        }) 
+        
+        if(!Comment) {
+
+            return res.status(400).json({ error: "Aucun Commentaire" });
+
+        }
+
+        return res.status(201).json(Comment);
+
+    } catch (error) {
+
+        return res.status(500).json({ message: error.message });
+        
+    }
+
+}
+
+
+
+// MODIFIER LE COMMENTAIRE //
+
+exports.modifyComment = async (req, res) => {
+
+    try {
+
+        const UserId = await jwt.getUserId(req);
+        const modifyComment = req.params.id;
+        
+        if(!req.body) {
+
+            return res.status(400).json({ error: "Ce commentaire ne peut pas être modifié" });
+
+        } 
+
+        
+            db.Comment.findOne({
+
+                where: { 
+                    id: modifyComment, 
+                    UserId: UserId 
+                },
+    
+            })
+
+
+        .then((Comment) => {
+
+            if(!Comment) {
+
+                return res.status(403).json({ error: "Vous n'avez pas l'authorisation ou le commentaire n'existe pas" });
+
+            } else {
+
+                return Comment.update({ content: req.body.content })
+
+            }
+
+        })
+
+
+        .then(() => {
+
+            return res.status(201).json({ message: "Le commentaire vient d'être modifié" });
+
+        })
+
+
+    } catch (error) {
+
+        return res.status(500).json({ message: error.message });
+        
+    }
+
+}
+
+
+
+
+
+// SUPPRIMER LE COMMENTAIRE //
+
+exports.deleteComment = async (req, res) => {
+
+    try {
+        const UserId = await jwt.getUserId(req);
+        const deleteComment = req.params.id;
+
+        if(!req.body) {
+
+            return res.status(400).json({ error: "Le commentaire à été supprimé !"})
+
+        }
+
+
+        db.Comment.findOne({
+
+            where: { id: deleteComment, UserId: UserId },
+
+        })
+
+
+        .then((deleteComment) => {
+
+            if(!deleteComment) {
+
+                return res.status(403).json({ error: "Vous n'avez pas l'authorisation"})
+
+            } else {
+
+                return deleteComment.destroy({})
+
+            }
+
+        })
+
+        .then(() => {
+
+            return res.status(201).json({ message: "Votre commentaire a été supprimé !"})
+
+        })
+
+        
+    } catch (error) {
+
+        return res.status(500).json({ message: error.message });
+        
+    }
+
+}
---
+++ back/userCtrl.js
@@ -0,0 +1,105 @@
+
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("../middleware/jwt");
+
+
+// S'INSCRIRE //
+
+exports.signup = async (req, res) => {
+
+    try {
+
+        const firstname = req.body.firstname;
+        const lastname = req.body.lastname;
+        const email = req.body.email;
+        let password = req.body.password;
+        let user = await db.User.findOne({ where: { 
+            
+            firstname, 
+            lastname, 
+            email 
+        }   
+        
+    });
+
+
+        if(user) {
+
+            return res.status(400).json({ error: "Utilisateur déjà Existant" });
+
+        }
+
+        // ENCRYPTER LE MOT DE PASSE //
+
+        password = await bcrypt.hash(password, 5);
+
+        user = await db.User.create({
+
+            firstname,
+            lastname,
+            email, 
+            password,
+
+        });
+
+        const token = jwt.generateToken(user);
+        return res.status(200).json({ token, message: "Utilisateur Créer" });
+
+        
+
+    } catch (error) {
+
+        return res.status(500).json({ error })
+        
+    }
+
+}
+
+
+
+    // SE CONNECTER //
+
+exports.login = async (req,res) => {
+
+    try {
+
+        const email = req.body.email;
+        const password = req.body.password;
+        let User = await db.User.findOne({ 
+            
+            where:{ 
+
+            email 
+        
+        } 
+
+    });
+
+
+        if(!User) {
+
+            return res.status(400).json( { error: "Utilisateur Inexistant"});
+
+        }
+
+        const isCorrectPassword = await bcrypt.compare(password, User.password);
+
+        if(!isCorrectPassword) {
+
+            return res.status(405).json({ error: "Mot de Passe Invalide !" });
+
+        }
+
+        const token = jwt.generateToken(User);
+        return res.status(200).json({ token, userId: User.id });
+
+
+        
+
+    } catch (error) {
+        
+        return res.status(500).json({ message: error.message })
+    }
+
+}
