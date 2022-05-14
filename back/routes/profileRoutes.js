a984e6c3ab8aee739bc0bbca09dde81214e99db7
Flavien <60539775+Livinien@users.noreply.github.com>
2022-05-14 17:53:35 +0200

    Add files via upload

--- back/routes/profileRoutes.js
+++ back/routes/profileRoutes.js
@@ -5,8 +5,17 @@ const auth = require("../middleware/jwt").auth;
 const multer = require('../middleware/multer-config');
 
 
+// ROUTES POUR POSTER L'IMAGE //
+
 router.post("/", auth, multer, profileCtrl.postImage);
-router.get("/", auth, profileCtrl.getImage);
+router.get("/", auth, profileCtrl.getProfile);
+
+
+
+// ROUTES POUR POSTER LE PRÉNOM ET LA DESCRIPTION //
+
+router.put("/", auth, multer, profileCtrl.modifyBiography);
+
 
 
 
